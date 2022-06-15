6600dfaf0ded92bb4224b2ffa996b13453c97e34
maltomann <cy.tsai.17@gmail.com>
2022-06-16 01:11:31 +0800

    searchlight swing. No detection methods yet.

--- assets/scripts/searchlight.ts
+++ assets/scripts/searchlight.ts
@@ -12,6 +12,8 @@ export default class searchlight extends cc.Component {
 
     @property()
     searchlight_speed: number = 10;
+    @property()
+    range: number = 50;
 
     @property(cc.ParticleSystem)
     spark : cc.ParticleSystem = null;
@@ -19,31 +21,43 @@ export default class searchlight extends cc.Component {
     private vis_time: number = 0;
     private attack: boolean = false;
 
+    private dir: number = 1;
+    private leftbound: number = 0;
+    private rightbound: number = 0;
+
     @property(cc.Node)
     eye_pos : cc.Node = null;
+    @property(cc.Node)
+    lightbeam : cc.Node = null;
     // LIFE-CYCLE CALLBACKS:
     private physicManager: cc.PhysicsManager = null;
     onLoad () {
         this.physicManager = cc.director.getPhysicsManager();
         this.physicManager.enabled = true;
-        this.wander();
+        // this.wander();
     }
 
     start () {
         //this.node.getComponent(cc.RigidBody).linearVelocity = cc.v2(this.searchlight_speed,0);
+        this.leftbound = this.node.x - this.range;
+        this.rightbound = this.node.x + this.range;
     }
 
     update (dt) {
-        //this.node.x+= this.searchlight_speed * dt;
+        this.node.x += this.searchlight_speed * dt * this.dir;
+        this.lightbeam.x += this.searchlight_speed * dt * this.dir / (2 * this.range/40);
+        this.eye_pos.angle += this.dir * 0.7 * this.range/50 / (this.searchlight_speed/60);
+        this.lightbeam.angle += this.dir * 0.2 * this.range/50 / (this.searchlight_speed/60);
+        if(this.node.x <= this.leftbound || this.node.x >= this.rightbound) this.dir *= -1;
     }
-    wander(){
-        cc.tween(this.node).repeatForever(
-            cc.tween(this.node)
-            .by(1, {x: this.node.position.x-100})  
-            .by(1, {x: this.node.position.x+100})
-        ).start();
+    // wander(){
+    //     cc.tween(this.node).repeatForever(
+    //         cc.tween(this.node)
+    //         .by(1, {x: this.node.position.x-100})  
+    //         .by(1, {x: this.node.position.x+100})
+    //     ).start();
         
-    }
+    // }
 
     ////////////////////////////////// TODO //////////////////////////////////
     // edge detection: time the amount of time the player takes from appearing in light range to eyes closing (vis_time)
--- library/imports/62/62469ddb-e363-4636-b98f-c9ccf13dfcbe.js
+++ library/imports/62/62469ddb-e363-4636-b98f-c9ccf13dfcbe.js
@@ -35,13 +35,25 @@ var searchlight = /** @class */ (function (_super) {
     function searchlight() {
         var _this = _super !== null && _super.apply(this, arguments) || this;
         _this.searchlight_speed = 10;
+        _this.range = 50;
         _this.spark = null;
         _this.vis_time = 0;
         _this.attack = false;
+        _this.dir = 1;
+        _this.leftbound = 0;
+        _this.rightbound = 0;
         _this.eye_pos = null;
+        _this.lightbeam = null;
         // LIFE-CYCLE CALLBACKS:
         _this.physicManager = null;
         return _this;
+        // wander(){
+        //     cc.tween(this.node).repeatForever(
+        //         cc.tween(this.node)
+        //         .by(1, {x: this.node.position.x-100})  
+        //         .by(1, {x: this.node.position.x+100})
+        //     ).start();
+        // }
         ////////////////////////////////// TODO //////////////////////////////////
         // edge detection: time the amount of time the player takes from appearing in light range to eyes closing (vis_time)
         // (t == 0): just move away
@@ -53,28 +65,36 @@ var searchlight = /** @class */ (function (_super) {
     searchlight.prototype.onLoad = function () {
         this.physicManager = cc.director.getPhysicsManager();
         this.physicManager.enabled = true;
-        this.wander();
+        // this.wander();
     };
     searchlight.prototype.start = function () {
         //this.node.getComponent(cc.RigidBody).linearVelocity = cc.v2(this.searchlight_speed,0);
+        this.leftbound = this.node.x - this.range;
+        this.rightbound = this.node.x + this.range;
     };
     searchlight.prototype.update = function (dt) {
-        //this.node.x+= this.searchlight_speed * dt;
-    };
-    searchlight.prototype.wander = function () {
-        cc.tween(this.node).repeatForever(cc.tween(this.node)
-            .by(1, { x: this.node.position.x - 100 })
-            .by(1, { x: this.node.position.x + 100 })).start();
+        this.node.x += this.searchlight_speed * dt * this.dir;
+        this.lightbeam.x += this.searchlight_speed * dt * this.dir / (2 * this.range / 40);
+        this.eye_pos.angle += this.dir * 0.7 * this.range / 50 / (this.searchlight_speed / 60);
+        this.lightbeam.angle += this.dir * 0.2 * this.range / 50 / (this.searchlight_speed / 60);
+        if (this.node.x <= this.leftbound || this.node.x >= this.rightbound)
+            this.dir *= -1;
     };
     __decorate([
         property()
     ], searchlight.prototype, "searchlight_speed", void 0);
+    __decorate([
+        property()
+    ], searchlight.prototype, "range", void 0);
     __decorate([
         property(cc.ParticleSystem)
     ], searchlight.prototype, "spark", void 0);
     __decorate([
         property(cc.Node)
     ], searchlight.prototype, "eye_pos", void 0);
+    __decorate([
+        property(cc.Node)
+    ], searchlight.prototype, "lightbeam", void 0);
     searchlight = __decorate([
         ccclass
     ], searchlight);
